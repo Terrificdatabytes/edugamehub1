607042a2914a3f92774c3bacdb71589d307ff07b
PRAVEEN K.S <66877413+Terrificdatabytes@users.noreply.github.com>
2025-09-14 15:55:07 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles/globals.css'
+import './index.css'
 
 // Error boundary component
 class ErrorBoundary extends React.Component<
